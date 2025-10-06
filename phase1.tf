d71b855a58731d55a7c376a5ae3879ab3fdf6b0e
bhavadharani <Bhavadharani.M@kyndryl.com>
2025-10-06 18:04:06 +0530

    phase4

--- phase1.tf
+++ phase1.tf
@@ -207,8 +207,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_sse" {
   }
 }
 
-resource "aws_dynamodb_table_2" "tf_lock_table" {
-  name         = "terraformlocks"
+resource "aws_dynamodb_table" "tf_lock_table" {
+  name         = "terraformlocks_21"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
