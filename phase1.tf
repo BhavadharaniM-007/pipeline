491bdc263d08d6714c6d23fb43da9b53845c8896
bhavadharani <Bhavadharani.M@kyndryl.com>
2025-10-08 12:24:33 +0530

    changed the folder name

--- phase1.tf
+++ phase1.tf
@@ -19,7 +19,7 @@ data "aws_vpc" "existing" {
 # Subnets
 resource "aws_subnet" "public_az1" {
   vpc_id                  = data.aws_vpc.existing.id
-  cidr_block              = "10.0.30.0/24"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
 
@@ -31,7 +31,7 @@ resource "aws_subnet" "public_az1" {
 data "aws_subnet" "public_az2" {
   filter {
     name   = "cidr-block"
-    values = ["10.0.20.0/24"]
+    values = ["10.0.2.0/24"]
   }
 
   filter {
